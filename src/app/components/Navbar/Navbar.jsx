da6cbc2a2b2a738de815324c9838c05c485dc1d1
love olubummo <olubummolove3@gmail.com>
2024-01-25 11:34:00 +0100

    made nav responsive

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import './styles.scss'
 import Link from 'next/link'
 const Navbar = () => {
     const [activeLink, setActiveLink] = useState('/');
+    const [ham, setHam] = useState(true)
 
   return (
     <nav className='nav-container'>
@@ -17,27 +18,33 @@ const Navbar = () => {
                 B.Furniture
             </span>
         </div>
-        <div className="nav-section">
+        <div className={`${ham? "nav-section" : "ham-nav"}`}>
             <Link href='/' className={`${activeLink === '/'? 'active-nav' : "" }`} onClick={(e)=>{setActiveLink(e.target.getAttribute('href'))
-        console.log(activeLink)
-        }}>
+             console.log(activeLink)
+             }}>
                 Home
             </Link>
             <Link href='/about' className={`${activeLink === '/about'? 'active-nav' : "" }`} onClick={(e)=>{setActiveLink(e.target.getAttribute('href'))
-        console.log(activeLink)
-        }}>
+            console.log(activeLink)
+            }}>
                 About Us
             </Link>
             <Link href='/gallery' className={`${activeLink === '/gallery'? 'active-nav' : "" }`} onClick={(e)=>{setActiveLink(e.target.getAttribute('href'))
-        console.log(activeLink)
-        }}>
+            console.log(activeLink)
+            }}>
                 Gallery
             </Link>
             <Link href='#footer' className={`${activeLink === '#footer'? 'active-nav' : "" }`} onClick={(e)=>{setActiveLink(e.target.getAttribute('href'))
-        console.log(activeLink)
-        }}>
+            console.log(activeLink)
+            }}>
                 Contact Us
             </Link>
+        </div>
+        <div className="hamContainer"
+        onClick={()=>setHam(!ham)}>
+            <div className={`${ham? "ham" : "ham-collapse"}`}>
+
+            </div>
         </div>
     </nav>
   )
