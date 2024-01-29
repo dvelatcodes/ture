2807d3a4de6d699bc5c2f6d1fa0b0e527e65f2fa
love olubummo <olubummolove3@gmail.com>
2024-01-29 20:35:27 +0100

    created inquiry section

--- src/app/homeComponents/Inquiry/Inquiry.jsx
+++ src/app/homeComponents/Inquiry/Inquiry.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import './styles.scss'
+import { Button } from '@/app/generalComponents';
+import images from '@/app/assets';
+import Image from 'next/image';
 const Inquiry = () => {
   return (
-    <div>Inquiry</div>
+    <div className='inquiry'>
+      <div className="inquiry-left-cover">
+          <div className="inquiry-left">
+          <h4>
+            Drop us a message
+          </h4>
+          <form action="#">
+            <label htmlFor="name">
+              Name
+            </label>
+            <input type="text" name="name" />
+            <label htmlFor="dial">
+              Contact Number
+            </label>
+            <input type="number" name="dial" />
+            <label htmlFor="message">
+              Message
+            </label>
+            <textarea name="message" cols="30" rows="10"></textarea>
+            <Button text='Submit' className='primary-button' type='button'/>
+          </form>
+        </div>
+      </div>
+      
+      <div className="inquiry-right">
+        <Image src={images.inputImage} alt='image input' width={413} height={748.212} placeholder='blur' blurDataURL='/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Finput-image.4c2db098.png&w=4&q=70'/>
+      </div>
+    </div>
   )
 }
 
