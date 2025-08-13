d7555b9d44f88846da597b6d8cbea4d382aa6113
love olubummo <olubummolove3@gmail.com>
2025-08-13 17:48:46 +0100

    fixed api location

--- src/app/components/Cart/Cart.jsx
+++ src/app/components/Cart/Cart.jsx
@@ -24,7 +24,7 @@ const Cart = () => {
 
   const handleCheckout = async () => {
     try {
-      const response = await fetch("/api/checkout_sessions", {
+      const response = await fetch("https://ture-f4ve5pz2u-dvelatcodes-projects.vercel.app/api/checkout_sessions", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
